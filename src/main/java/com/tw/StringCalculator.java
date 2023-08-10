a816094f7fa5f3e3b6190942a4f57dcc78f7b6c3
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-08-10 18:55:59 +0800

    (Java exam): Add implementation code for existing unit tests

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,18 @@
 package com.tw;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        return Arrays.stream(string.split("[^0-9]")).filter(element -> {
+                    try {
+                        Integer.parseInt(element);
+                        return true;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                }).map(element -> Integer.parseInt(element))
+                .reduce((num1, num2) -> Integer.sum(num1, num2))
+                .orElse(0);
     }
 }
